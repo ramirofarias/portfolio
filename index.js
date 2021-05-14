90af61a513d5e1e984a971e7a69c68eb8ac2365a
Ramiro Farias <ramirofarias.dev@gmail.com>
2021-05-14 16:40:13 -0300

    Fixed dark mode affecting pic

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const darkModeButton = document.getElementById("dark");
 
 const toggleDarkMode = () => {
   document.documentElement.classList.toggle("dark");
+    document.querySelectorAll('.inverted').forEach((el) => {
+      el.classList.toggle('invert')
+    })
+
 };
 
 darkModeButton.addEventListener("click", toggleDarkMode);
